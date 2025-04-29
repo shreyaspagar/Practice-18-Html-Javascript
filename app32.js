6b5c8359cdc02eda2a36b90c1bf91f67ba8ac736
Shreyas Pagar <130757999+shreyaspagar@users.noreply.github.com>
2025-04-29 19:10:00 +0530

    Add files via upload

---
+++ app32.js
@@ -0,0 +1,9 @@
+let array = ["hello" , "a" , "5" , "9" , "64"];
+let item = "64";
+
+if (array.indexOf(item)  != -1) {
+    console.log("elements exists in a array");
+}
+else {
+    console.log("elements does not exists in a array");
+}
